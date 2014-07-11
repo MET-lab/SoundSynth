2cba7cb3373a172167380c00a8dfdf08476f1650
JeffGregorio <gregorio.jeff@gmail.com>
2014-07-11 13:41:00 -0400

    Initial Commit

---
+++ SoundSynth/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SoundSynth
+//
+//  Created by Jeff Gregorio on 7/11/14.
+//  Copyright (c) 2014 Jeff Gregorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SoundSynth/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SoundSynth
+//
+//  Created by Jeff Gregorio on 7/11/14.
+//  Copyright (c) 2014 Jeff Gregorio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
